aed9d3d89df254a6dbe0f020a22f60df4c572a71
JonaS94 <jonas94@MacBook-Pro-de-JonaS94.local>
2025-05-05 20:24:20 -0500

    Primer Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import freeeCodeCampLogo from "./imagenes/freecodecamplogo.png";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="frecodecamp-logo-contendor">
+        <img
+          src={freeeCodeCampLogo}
+          className="freecodecamp-logo"
+          alt="FreeCodeCamp Logo"
+        />
+        <div className="contenedor-calculadora"></div>
+      </div>
     </div>
   );
 }
